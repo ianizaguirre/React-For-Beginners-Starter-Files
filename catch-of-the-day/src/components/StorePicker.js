e2e30463db62567e96b17d1cd065beedd990fecf
[ianizaguirre] <ian@izaguir.re>
2018-03-14 16:46:03 -0400

    Add: StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+	render() {
+		return (
+			<div>
+				<p>I Am The Store Picker</p>
+			</div>
+		);
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+const renderApp = () => {
+	render(<StorePicker />, document.querySelector('#main'));
+};
+
+renderApp();
