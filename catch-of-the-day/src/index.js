29ff627d75c81e066d5d04600b90111ff9afdedd
[ianizaguirre] <ian@izaguir.re>
2018-03-14 19:22:45 -0400

    Add: created application layout with components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,9 +3,10 @@ import { render } from 'react-dom';
 import "./css/style.css";
 
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 
 const renderApp = () => {
-	render(<StorePicker />, document.querySelector('#main'));
+	render(<App />, document.querySelector('#main'));
 };
 
 renderApp();
