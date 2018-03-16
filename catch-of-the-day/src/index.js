64accbeccc0891c94ce35c0f9ff6f62f96b3818a
[ianizaguirre] <ian@izaguir.re>
2018-03-16 03:08:55 -0400

    Add: set up BrowserRouter, 404, and refactored some

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import "./css/style.css";
+import './css/style.css';
 
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 
 const renderApp = () => {
-	render(<App />, document.querySelector('#main'));
+	render(<Router />, document.querySelector('#main'));
 };
 
 renderApp();
