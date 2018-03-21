23102db04f737daa81a3e403e5da736c5f41d8e8
[ianizaguirre] <ian@izaguir.re>
2018-03-21 06:55:46 -0400

    Update: creating the basic layout of the AddFishForm component

--- catch-of-the-day/src/components/AddFishForm.jsx
+++ catch-of-the-day/src/components/AddFishForm.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class AddFishForm extends Component {
+	createFish = event => {
+		event.preventDefault();
+		console.log('CREATE FISH FUNCTION');
+	};
+
 	render() {
 		return (
-			<form>
-				<p>It Works!!</p>
+			<form className="fish-edit" onSubmit={this.createFish}>
+				<input name="name" type="text" placeholder="Name" />
+				<input name="price" type="text" placeholder="Price" />
+
+				<select name="status">
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+
+				<textarea name="desc" placeholder="Desc" />
+				<input name="image" type="text" placeholder="Image" />
+				<button type="submit">+ Add Fish </button>
 			</form>
 		);
 	}
