281afed0fcf44c89eb2083951480e6354371e7de
[ianizaguirre] <ian@izaguir.re>
2018-03-21 05:50:05 -0400

    Add: fish form component

---
+++ catch-of-the-day/src/components/AddFishForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddFishForm extends Component {
+	render() {
+		return (
+			<form>
+				<p>It Works!!!</p>
+			</form>
+		);
+	}
+}
+
+export default AddFishForm;
